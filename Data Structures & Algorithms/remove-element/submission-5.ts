ac34982c8b230df2ff0314c342706ea34b42829d
Luis Rodriguez III <luisrodrigueziii.dev@gmail.com>
2026-09-01 14:21:48 -0400

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.ts
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] === val) {
+                nums.splice(i, 1, 999);
+            }
+        }
+        nums.sort();
+        return nums.filter((num) => num !== 999).length;
+    }
+}
