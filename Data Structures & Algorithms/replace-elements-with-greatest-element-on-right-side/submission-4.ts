ba132ee78eea4e9fd0de0866e66d9794edf351d7
Luis Rodriguez III <luisrodrigueziii.dev@gmail.com>
2026-09-01 16:51:40 -0400

    Add: replace-elements-with-greatest-element-on-right-side - submission-4

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-4.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr: number[]): number[] {
+        let max: number = -1;     
+        for (let i = arr.length -1; i > -1; i--) {
+            let newMax: number = Math.max(max, arr[i]);
+            arr[i] = max;
+            max = newMax;
+        }
+        return arr;
+    }
+}
