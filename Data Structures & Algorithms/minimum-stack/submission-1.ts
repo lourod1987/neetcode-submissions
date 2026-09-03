1d4ec2127b9092861c1ebe034b59bd23534d3298
Luis Rodriguez III <luisrodrigueziii.dev@gmail.com>
2026-09-03 02:22:04 -0400

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.ts
@@ -0,0 +1,44 @@
+class MinStack {
+    constructor() {}
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    arr: number[] = [];
+    minStack: number[] = [];
+    push(val: number): void {
+        this.arr.push(val);
+        if (val <= this.minStack[this.minStack.length -1] || this.minStack.length === 0) {
+            this.minStack.push(val);
+        }
+        // console.log(this.arr);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop(): void {
+        if (this.arr[this.arr.length - 1] === this.minStack[this.minStack.length - 1]) {
+            this.minStack.pop();
+            this.arr.pop();
+        } else {
+            this.arr.pop();
+        }
+        // console.log(this.arr);
+    }
+
+    /**
+     * @return {number}
+     */
+    top(): number {
+        return this.arr[this.arr.length - 1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}
