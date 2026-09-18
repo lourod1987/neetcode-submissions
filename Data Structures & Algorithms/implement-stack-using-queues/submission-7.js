095301644d56cca5744814b7cf24eee3a479d27f
Luis Rodriguez III <luisrodrigueziii.dev@gmail.com>
2026-09-18 00:04:40 -0400

    Add: implement-stack-using-queues - submission-7

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-7.js
@@ -0,0 +1,51 @@
+class MyStack {
+    constructor() {
+        this.reverseQueue = new Queue();
+    }
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    push(x) {
+        this.reverseQueue.push(x);
+        let length = this.reverseQueue.size();
+        while (length > 1) {
+            this.reverseQueue.push(this.reverseQueue.pop());
+            length --;
+        }
+    }
+
+    /**
+     * @return {number}
+     */
+    pop() {
+        return this.reverseQueue.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.reverseQueue.front();
+    }
+
+    /**
+     * @return {boolean}
+     */
+    empty() {
+        if (this.reverseQueue.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
